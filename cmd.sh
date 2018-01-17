81c808750da6bba51db27cd39543a2ba7457559b
takagotch <dyaccb@gmail.com>
2018-01-17 23:59:31 +0900

    package.json

---
+++ cmd.sh
@@ -0,0 +1,10 @@
+bower install 
+bower install jquery
+bower install desandro/masonry
+bower install git:github.com/user/package.git
+bower install http://ex.com/script.js
+
+bower init
+bower install PACAGE --save
+
+
